8585a436ed5756aceae61c063a993e3f115d28b2
1lyasm <1lyasm.dev@gmail.com>
2024-02-21 19:07:54 +0300

    Add regexp matching to gin

--- user/find.c
+++ user/find.c
@@ -4,6 +4,50 @@
 #include "kernel/fcntl.h"
 #include "user/user.h"
 
+// Regexp matcher from Kernighan & Pike,
+// The Practice of Programming, Chapter 9, or
+// https://www.cs.princeton.edu/courses/archive/spr09/cos333/beautiful.html
+
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
+}
+
+
 char*
 fmtname(char *path)
 {
@@ -24,8 +68,8 @@ fmtname(char *path)
 }
 
 
-int find(char *dir, char *fname) {
-  /* printf("find: dir: %s, fname: %s\n", dir, fname); */
+int find(char *dir, char *regex) {
+  /* printf("find: dir: %s, regex: %s\n", dir, regex); */
   int retcode = 0;
   char buf[512];
   int fd;
@@ -59,8 +103,8 @@ int find(char *dir, char *fname) {
                 fprintf(2, "find: cannot stat %s\n", buf);
                 continue;
               }
-              /* printf("buf: %s, p: %s, fname: %s\n", buf, p, fname); */
-              if (strcmp(p, fname) == 0) {
+              /* printf("buf: %s, p: %s, regex: %s\n", buf, p, regex); */
+              if (match(regex, p) == 1) {
                 fprintf(1, "%s\n", buf);
               }
               if (strcmp(p, ".") != 0 && strcmp(p, "..") != 0) {
@@ -68,7 +112,7 @@ int find(char *dir, char *fname) {
                 char *bufcp = malloc((buflen + 1) * sizeof(char));
                 bufcp[buflen] = 0;
                 strcpy(bufcp, buf);
-                find(bufcp, fname);
+                find(bufcp, regex);
                 free(bufcp);
               }
             }
@@ -88,9 +132,9 @@ int find(char *dir, char *fname) {
 
 int main(int argc, char *argv[]) {
   char *dir = argv[1];
-  char *fname = argv[2];
-  /* printf("fname: %s\n", fname); */
+  char *regex = argv[2];
+  /* printf("regex: %s\n", regex); */
 
-  int retcode = find(dir, fname);
+  int retcode = find(dir, regex);
   exit(retcode);
 }
